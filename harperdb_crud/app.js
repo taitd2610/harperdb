3f4201637cfadb326f6f7a40ce8249811fabb65d
taitd153 <trinhdinhtai1996@gmail.com>
2021-07-21 11:26:24 +0700

    up

--- harperdb_crud/app.js
+++ harperdb_crud/app.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.send("Hello HarperDB, NodeJS server is up and running");
 });
 
-// Tạo mới 1 schema
+// Create Schema
 app.post("/create/schema", (req, res) => {
   const { body } = req;
 
@@ -28,16 +28,139 @@ app.post("/create/schema", (req, res) => {
   );
 });
 
-// Tạo mới 1 table
+// Create Table
 app.post("/create/table", (req, res) => {
   const { body } = req;
 
-  db.createSchema(
+  db.createTable(
     {
       operation: "create_table",
       schema: body.schema,
       table: body.table,
       hashAttribute: "id",
+    },
+    (err, response) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      res.status(response.statusCode).json(response.data);
+    }
+  );
+});
+
+// Create Data in movie table
+app.post("/movie/action/create", (req, res) => {
+  const { body } = req;
+
+  db.insert(
+    {
+      operation: "insert",
+      schema: body.schema,
+      table: body.table,
+      records: body.records,
+    },
+    (err, response) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      res.status(response.statusCode).json(response.data);
+    }
+  );
+});
+
+// Update
+app.post("/movie/action/update", (req, res) => {
+  const { body } = req;
+
+  db.update(
+    {
+      operation: "update",
+      schema: body.schema,
+      table: body.table,
+      records: body.records,
+    },
+    (err, response) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      res.status(response.statusCode).json(response.data);
+    }
+  );
+});
+
+// Upsert a new row
+app.post("/movie/action/upsert", (req, res) => {
+  const { body } = req;
+
+  db.upsert(
+    {
+      operation: "upsert",
+      schema: body.schema,
+      table: body.table,
+      searchAttribute: body.records,
+    },
+    (err, response) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      res.status(response.statusCode).json(response.data);
+    }
+  );
+});
+
+// Get all records
+app.post("/movie/action/search", (req, res) => {
+  const { body } = req;
+
+  db.searchByHash(
+    {
+      operation: "search_by_hash",
+      schema: body.schema,
+      table: body.table,
+      hashValues: body.hashValues,
+      attributes: body.getAttributes, // column get
+    },
+    (err, response) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      res.status(response.statusCode).json(response.data);
+    }
+  );
+});
+
+// Get records by values
+app.post("/movie/action/searchByValue", (req, res) => {
+  const { body } = req;
+
+  db.searchByValue(
+    {
+      operation: "search_by_value",
+      schema: body.schema,
+      table: body.table,
+      searchAttribute: body.searchAttribute,
+      searchValue: body.searchValue,
+      attributes: body.getAttributes, // column get
+    },
+    (err, response) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+      res.status(response.statusCode).json(response.data);
+    }
+  );
+});
+
+// Delete records
+app.post("/movie/action/delete", (req, res) => {
+  const { body } = req;
+
+  db.delete(
+    {
+      operation: "delete",
+      schema: body.schema,
+      table: body.table,
+      hashValues: body.hashValues, // column get
     },
     (err, response) => {
       if (err) {
